6a3c701a0112a9e020df4a029d1495ed662eabc8
milkybar1910 <sivashankar1326@gmail.com>
2020-11-11 23:29:49 +0530

    Project 1 and Project 2

---
+++ BGCHANGER/App.js
@@ -0,0 +1,68 @@
+import React, {useState} from 'react';
+import {StyleSheet, Text, TouchableOpacity, View,StatusBar} from 'react-native';
+
+const App = () => {
+  const [randomColor, setRandomColor] = useState('rgb(32,0,126)');
+
+  const changeBG =()=>{
+    let color = 'rgb('+
+    Math.floor(Math.random() *256) +
+      ','+
+      Math.floor(Math.random() *256)+
+      ','+
+      Math.floor(Math.random() *256)+
+      ')';
+      setRandomColor(color)
+  }
+
+  const changeBlack =() =>{
+    setRandomColor('#111111')
+  }
+  return (
+
+    <>
+    <StatusBar backgroundColor={randomColor} />
+    <View style={[styles.container, {backgroundColor: randomColor}]}>
+    <TouchableOpacity onPress={changeBG}>
+      <Text style={styles.text}>RANDOM BG</Text>
+      </TouchableOpacity>
+
+    <TouchableOpacity onPress={changeBlack}>
+      <Text style={styles.text2}>DARK MODE</Text>
+
+      </TouchableOpacity>
+    </View>
+    </>
+    );
+};
+
+export default App;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text: {
+    fontSize: 30,
+    backgroundColor: '#BB2CD9',
+    paddingVertical: 10,
+    paddingHorizontal: 40,
+    color: '#FFFFFF',
+    borderRadius: 15,
+    textTransform: 'uppercase',
+  },
+  text2: {
+    marginTop:15,
+    fontSize: 30,
+    
+    paddingVertical: 10,
+    paddingHorizontal: 40,
+    color: '#FFFFFF',
+    borderRadius: 15,
+    textTransform: 'uppercase',
+    backgroundColor:"#111111"
+  },
+});
